2d001d49a91b5da5cff792ec2767323f9ccfd425
Robert <treboryx@gmail.com>
2020-08-24 14:19:06 +0300

    new restricted google api key, installed vue-ua for analytics instead

--- src/main.js
+++ src/main.js
@@ -4,22 +4,23 @@ import VueGeolocation from "vue-browser-geolocation";
 import GmapCluster from "vue2-google-maps/dist/components/cluster";
 Vue.component("cluster", GmapCluster);
 import "./assets/css/main.css";
-import VueAnalytics from "vue-analytics";
+import VueAnalytics from "vue-ua";
 import * as VueGoogleMaps from "vue2-google-maps";
 Vue.config.productionTip = false;
 Vue.use(VueGeolocation);
 
 Vue.use(VueGoogleMaps, {
   load: {
-    key: "AIzaSyAMkg5eswyQmqDEc6gkLfq8608cm6UCIWI",
+    key: "AIzaSyBg4wE12cEcuWJGQMbKNnb6m2CUTydSZSY",
     libraries: ["places", "geometry"],
   },
 });
 
 Vue.use(VueAnalytics, {
-  id: "UA-129649039-2",
+  appName: "fttx",
+  appVersion: "1.0.0",
+  trackingId: "UA-129649039-2",
 });
-
 new Vue({
   render: (h) => h(App),
 }).$mount("#app");
