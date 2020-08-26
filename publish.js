afc65723d1fe64eedcaf509ffa26e8470c33f991
Robert <treboryx@gmail.com>
2020-08-26 03:19:22 +0300

    modified publish.js

--- publish.js
+++ publish.js
@@ -1,5 +1,5 @@
 const ghpages = require("gh-pages");
-
+console.log("Publishing frontend to GitHub Pages...");
 ghpages.publish("dist", function(err) {
-  console.log("Publishing frontend to GitHub Pages...");
+  console.log("Published!");
 });
